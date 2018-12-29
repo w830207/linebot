7c1a6ee24a2167468620685580277a845cc61435
w830207 <44136372+w830207@users.noreply.github.com>
2018-12-29 15:20:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -61,45 +61,7 @@ def handle_message(event):
     if event.message.text != "" && event.message.text != "喝茶":
 			message = TextSendMessage(text='嗨帥哥你好！輸入"喝茶"提供服務哦！')
 			line_bot_api.reply_message(event.reply_token, message)
-	if event.message.text == "喝茶":
-        confirm_template_message = TemplateSendMessage(
-                            alt_text='請問您是否成年？', 
-                            template=ConfirmTemplate(
-                            text='請問您是否成年？',
-                            actions=[
-#                                PostbackTemplateAction 點擊選項後，
-#                                 除了文字會顯示在聊天室中，
-#                                 還回傳data中的資料，可
-#                                 此類透過 Postback event 處理。
-                                PostbackTemplateAction(
-                                    label='是', 
-                                    text='是',
-                                    data='year=1'
-                                ),
-                                PostbackTemplateAction(
-                                    label='否', 
-                                    text='否',
-                                    data='year=0'
-                                )
-                            ]
-							)
-						)
-        line_bot_api.reply_message( event.reply_token,confirm_template_message)
-	if event.message.text == "是":
-			message = TextSendMessage(text='請輸入服務地區 服務地區:北區 西屯區 中區')
-			line_bot_api.reply_message(event.reply_token, message)
-	if event.message.text == "西屯區":
-			message = TextSendMessage(text='test1')
-			line_bot_api.reply_message(event.reply_token, message)
-	elif	event.message.text == "北區":
-			message = TextSendMessage(text='test2')
-			line_bot_api.reply_message(event.reply_token, message)
-    elif	event.message.text == "北區":
-			message = TextSendMessage(text='test3')
-			line_bot_api.reply_message(event.reply_token, message)
-    else:
-			message = TextSendMessage(text='不好意思目前該地區不提供服務\n請輸入服務地區 服務地區:北區 西屯區 中區')
-			line_bot_api.reply_message(event.reply_token, message)
+
     
 import os
 if __name__ == "__main__":
