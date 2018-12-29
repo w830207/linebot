7071bf85ef7fecd72f8f44ae2325b74818b6184f
w830207 <44136372+w830207@users.noreply.github.com>
2018-12-29 14:59:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, abort
 import os
 import sys
+import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -32,7 +35,6 @@ handler = WebhookHandler(SECRET)
 
 
 
-
 @app.route("/")
 def hello_world():
     return "hello world!"
@@ -56,11 +58,57 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == "666":
-           message = TextSendMessage(text='55555')
-           line_bot_api.reply_message(event.reply_token, message)
-    
-    
+	year = 0
+    if event.message.text != "" && event.message.text != "喝茶" && year = 0:
+			message = TextSendMessage(text='嗨帥哥你好！輸入"喝茶"提供服務哦！')
+			line_bot_api.reply_message(event.reply_token, message)
+	if event.message.text == "喝茶":
+        button_template_message =ButtonsTemplate(
+                            thumbnail_image_url="https://i.imgur.com/eTldj2E.png?1",
+                            title='請問您是否成年？', 
+                            text='Please select',
+                            ratio="1.51:1",
+                            image_size="cover",
+                            actions=[
+#                                PostbackTemplateAction 點擊選項後，
+#                                 除了文字會顯示在聊天室中，
+#                                 還回傳data中的資料，可
+#                                 此類透過 Postback event 處理。
+                                PostbackTemplateAction(
+                                    label='是', 
+                                    text='是',
+                                    data='year=1'
+                                ),
+                                PostbackTemplateAction(
+                                    label='否', 
+                                    text='否',
+                                    data='year=0'
+                                )
+                            ]
+                        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text="Template Example",
+                template=button_template_message
+            )
+        )
+	if event.message.text == "是"&& year == 1 :
+			message = TextSendMessage(text='請輸入服務地區 服務地區:北區 西屯區 中區')
+			line_bot_api.reply_message(event.reply_token, message)
+	if event.message.text == "西屯區"&& year == 1 :
+			message = TextSendMessage(text='test1')
+			line_bot_api.reply_message(event.reply_token, message)
+	elif	event.message.text == "北區"&& year == 1 :
+			message = TextSendMessage(text='test2')
+			line_bot_api.reply_message(event.reply_token, message)
+    elif	event.message.text == "北區"&& year == 1 :
+			message = TextSendMessage(text='test3')
+			line_bot_api.reply_message(event.reply_token, message)
+    else:
+			message = TextSendMessage(text='不好意思目前該地區不提供服務\n請輸入服務地區 服務地區:北區 西屯區 中區')
+			line_bot_api.reply_message(event.reply_token, message)
     
 import os
 if __name__ == "__main__":
