f84b466f33c552bf2f6cb46ae7ac7abdc159558c
w830207 <44136372+w830207@users.noreply.github.com>
2019-01-02 00:35:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+	ttt=""
 	dictY = {'西屯區': 3, '中區': 4, '北區': 5,'東區': 2, '南區': 2, '西區': 2, '北屯區': 2, '南屯區': 2, '太平區': 2, '大里區': 2, '龍井區': 2, '沙鹿區': 2, '梧棲區': 2,'清水區': 2,'大甲區': 2,'霧峰區': 2, '烏日區': 2, '豐原區': 2,'后里區': 2, '石岡區': 2, '東勢區': 2, '和平區': 2, '新社區': 2, '潭子區': 2, '大雅區': 2, '神岡區': 2, '大雅區': 2, '大肚區': 2,'外埔區': 2,'大安區': 2}
 	if event.message.text not in dictY and event.message.text != "是" and event.message.text != "喝茶":
 			message = TextSendMessage(text='嗨帥哥你好！輸入"喝茶"提供服務哦！\n目前只有台中地區提供服務')
@@ -108,21 +109,21 @@ def handle_message(event):
 					print('無法連線Google試算表', ex)
 					sys.exit(1)
 				cell_list4 = worksheet.range('A4:E4')
-					ttt += str(cell_list4)
-					for i in range(8,18):
-					#8-18代表營業時間從1500-2400
-						if worksheet.cell(4, i).value =="" :
-							ttt += ' '+str(i+7)+'available'
-					message = TextSendMessage(text=ttt)
-					break
+				ttt += str(cell_list4)
+				for i in range(8,18):
+				#8-18代表營業時間從1500-2400
+					if worksheet.cell(4, i).value =="" :
+						ttt += ' '+str(i+7)+'available'
+				message = TextSendMessage(text=ttt)
+				break
 				cell_list5 = worksheet.range('A5:E5')
-					ttt += str(cell_list5)
-					for i in range(8,18):
-					#8-18代表營業時間從1500-2400
-						if worksheet.cell(5, i).value =="" :
-							ttt += ' '+str(i+7)+'available'
-					message = TextSendMessage(text=ttt)
-					break
+				ttt += str(cell_list5)
+				for i in range(8,18):
+				#8-18代表營業時間從1500-2400
+					if worksheet.cell(5, i).value =="" :
+						ttt += ' '+str(i+7)+'available'
+				message = TextSendMessage(text=ttt)
+				break
 			line_bot_api.reply_message(event.reply_token, message4)
 	elif dictY[event.message.text]==5:
 			#'西屯區': 3, '中區': 4, '北區': 5
@@ -198,7 +199,6 @@ def handle_message(event):
 	elif dictY[event.message.text]==2:
 			message = TextSendMessage(text='不好意思目前該地區不提供服務\n請輸入服務地區 服務地區:北區 西屯區 中區')
 			line_bot_api.reply_message(event.reply_token, message)
-									
 
 
 import os
